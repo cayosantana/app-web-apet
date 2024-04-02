4397415ce9f3ce5c31bdacca7d482e971c675198
cayosantana <cc.santana.js@gmail.com>
2024-04-02 16:28:27 -0300

    Create Initial set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from "react"
+import styled from "styled-components";
+import { GlobalStyle } from "./style/globalStyles";
 
 function App() {
+
+  const Title = styled.h1`
+    color: orange;
+  `;
   
   return (
     <>
-      <h1>Teste Apet</h1>
+      <GlobalStyle/>
+      <Title>Teste Apet</Title>
+      
     </>
   )
 }
